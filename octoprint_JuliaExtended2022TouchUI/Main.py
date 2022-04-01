ab566845e51e39b53da0eda3489dda5a392e87bc
Vijay Raghav Varada <vjvarada@users.noreply.github.com>
2022-04-01 16:01:50 +0530

    added error checking in print restore

--- octoprint_JuliaExtended2022TouchUI/Main.py
+++ octoprint_JuliaExtended2022TouchUI/Main.py
@@ -1744,7 +1744,10 @@ class MainUiClass(QtWidgets.QMainWindow, mainGUI.Ui_MainWindow):
             os.system('sudo cp -f config/users.yaml /home/pi/.octoprint/users.yaml')
             os.system('sudo rm -rf /home/pi/.octoprint/printerProfiles/*')
             os.system('sudo rm -rf /home/pi/.octoprint/scripts/gcode')
-            os.system('sudo rm -rf /home/pi/.octoprint/print_restore.json')
+            try:
+                os.system('sudo rm -rf /home/pi/.octoprint/print_restore.json')
+            except:
+                pass
             os.system('sudo cp -f config/config.yaml /home/pi/.octoprint/config.yaml')
             # os.system('sudo rm -rf /home/pi/.fw_logo.dat')
             self.tellAndReboot("Settings restored. Rebooting...")
