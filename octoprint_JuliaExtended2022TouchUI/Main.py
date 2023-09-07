5c338ba400c9bd7af01757fd1847b1ecafec848e
Vijay Raghav Varada <vjvarada@users.noreply.github.com>
2023-09-07 18:01:15 +0530

    created "SafeProceed" for when Octoprint fails to load

--- octoprint_JuliaExtended2022TouchUI/Main.py
+++ octoprint_JuliaExtended2022TouchUI/Main.py
@@ -353,9 +353,119 @@ class MainUiClass(QtWidgets.QMainWindow, mainGUI.Ui_MainWindow):
         self.loadingGif.setMovie(self.movie)
         self.movie.start()
 
+
+    def safeProceed(self):
+        '''
+        When Octoprint server cannot connect for whatever reason, still show the home screen to conduct diagnostics
+        '''
+        self.movie.stop()
+        if not Development:
+            self.stackedWidget.setCurrentWidget(self.homePage)
+            # self.Lock_showLock()
+            self.setIPStatus()
+        else:
+            self.stackedWidget.setCurrentWidget(self.homePage)
+
+        # # Text Input events
+        self.wifiPasswordLineEdit.clicked_signal.connect(lambda: self.startKeyboard(self.wifiPasswordLineEdit.setText))
+        self.ethStaticIpLineEdit.clicked_signal.connect(lambda: self.ethShowKeyboard(self.ethStaticIpLineEdit))
+        self.ethStaticGatewayLineEdit.clicked_signal.connect(lambda: self.ethShowKeyboard(self.ethStaticGatewayLineEdit))
+
+        # Button Events:
+
+        # Home Screen:
+        self.stopButton.setDisabled()
+        # self.menuButton.pressed.connect(self.keyboardButton)
+        self.menuButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.MenuPage))
+        self.controlButton.setDisabled()
+        self.playPauseButton.setDisabled()
+
+        # MenuScreen
+        self.menuBackButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.homePage))
+        self.menuControlButton.setDisabled()
+        self.menuPrintButton.setDisabled()
+        self.menuCalibrateButton.setDisabled()
+        self.menuSettingsButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.settingsPage))
+
+
+        # Settings Page
+        self.networkSettingsButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.networkSettingsPage))
+        self.displaySettingsButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.displaySettingsPage))
+        self.settingsBackButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.MenuPage))
+        self.pairPhoneButton.pressed.connect(self.pairPhoneApp)
+        self.OTAButton.setDisabled()
+        self.versionButton.setDisabled()
+
+        self.restartButton.pressed.connect(self.askAndReboot)
+        self.restoreFactoryDefaultsButton.pressed.connect(self.restoreFactoryDefaults)
+        self.restorePrintSettingsButton.pressed.connect(self.restorePrintDefaults)
+
+        # Network settings page
+        self.networkInfoButton.pressed.connect(self.networkInfo)
+        self.configureWifiButton.pressed.connect(self.wifiSettings)
+        self.configureEthButton.pressed.connect(self.ethSettings)
+        self.networkSettingsBackButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.settingsPage))
+
+        # Network Info Page
+        self.networkInfoBackButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.networkSettingsPage))
+
+        # WifiSetings page
+        self.wifiSettingsSSIDKeyboardButton.pressed.connect(
+            lambda: self.startKeyboard(self.wifiSettingsComboBox.addItem))
+        self.wifiSettingsCancelButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.networkSettingsPage))
+        self.wifiSettingsDoneButton.pressed.connect(self.acceptWifiSettings)
+
+        # Ethernet setings page
+        self.ethStaticCheckBox.stateChanged.connect(self.ethStaticChanged)
+        # self.ethStaticCheckBox.stateChanged.connect(lambda: self.ethStaticSettings.setVisible(self.ethStaticCheckBox.isChecked()))
+        self.ethStaticIpKeyboardButton.pressed.connect(lambda: self.ethShowKeyboard(self.ethStaticIpLineEdit))
+        self.ethStaticGatewayKeyboardButton.pressed.connect(lambda: self.ethShowKeyboard(self.ethStaticGatewayLineEdit))
+        self.ethSettingsDoneButton.pressed.connect(self.ethSaveStaticNetworkInfo)
+        self.ethSettingsCancelButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.networkSettingsPage))
+
+        # Display settings
+        self.rotateDisplay.pressed.connect(self.showRotateDisplaySettingsPage)
+        self.calibrateTouch.pressed.connect(self.touchCalibration)
+        self.displaySettingsBackButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.settingsPage))
+
+        # Rotate Display Settings
+        self.rotateDisplaySettingsDoneButton.pressed.connect(self.saveRotateDisplaySettings)
+        self.rotateDisplaySettingsCancelButton.pressed.connect(
+            lambda: self.stackedWidget.setCurrentWidget(self.displaySettingsPage))
+
+        # QR Code
+        self.QRCodeBackButton.pressed.connect(lambda: self.stackedWidget.setCurrentWidget(self.settingsPage))
+
+        # SoftwareUpdatePage
+        self.softwareUpdateBackButton.setDisabled()
+        self.performUpdateButton.setDisabled()
+
+        # Firmware update page
+        self.firmwareUpdateBackButton.setDisabled()
+
+        # Filament sensor toggle
+        self.toggleFilamentSensorButton.setDisabled()
+
+
+    def handleStartupError(self):
+        '''
+        Error Handler when Octoprint gives up
+        '''
+        self.safeProceed()
+        print('Unable to connect to Octoprint Server')
+        if dialog.WarningOk(self, "Unable to connect to internal Server, try restoring factory settings", overlay=True):
+            pass
+
+
+
     def proceed(self):
         '''
-        Startes websocket, as well as initialises button actions and callbacks. THis is done in such a manner so that the callbacks that dnepend on websockets
+        Startes websocket, as well as initialises button actions and callbacks. This is done in such a manner so that the callbacks that dnepend on websockets
         load only after the socket is available which in turn is dependent on the server being available which is checked in the sanity check thread
         '''
         self.QtSocket = QtWebsocket()
@@ -1807,10 +1917,7 @@ class MainUiClass(QtWidgets.QMainWindow, mainGUI.Ui_MainWindow):
             return True
         return False
 
-    def handleStartupError(self):
-        print('Shutting Down. Unable to connect')
-        if dialog.WarningOk(self, "Error. Contact Support. Shutting down...", overlay=True):
-            os.system('sudo shutdown now')
+
 
     def pairPhoneApp(self):
         if getIP(ThreadRestartNetworking.ETH) is not None:
